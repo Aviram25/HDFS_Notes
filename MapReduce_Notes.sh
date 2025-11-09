e1f1a9503f6e65e0bb41411a10709b199274da67
Aviram Dhagat <dhagataviram@gmail.com>
2025-11-09 23:26:06 +0530

    Update MapReduce_Notes.sh

--- MapReduce_Notes.sh
+++ MapReduce_Notes.sh
@@ -2,6 +2,12 @@ jar -tf file.jar
 
 yarn jar file.jar package.class
 
-yarn logs -applicationId application_1234_12
+yarn jar my-jar.jar mypkg.myDriverClass input-file output-file -D mapreduce.job.reduces=10
 
+yarn logs -applicationId application_123_12
 
+java -cp "../build/project1.jar:/usr/lib/hadoop/client/*:../mysql-connector-java1-5.148.jar" hdfs-DataLoad
+
+javac -cp "/usr/lib/hadoop/client/" -d . /src/hdfs/.java
+
+java -cvf /build/project2.jar /hdfs
